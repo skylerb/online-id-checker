fa74185f73fd9ee0299ecfc20bdc9abf905ea473
Skyler <sbeckman@purdue.edu>
2013-03-20 22:31:09 -0400

    Removed hardcoding in jquery code

--- webapps/idcheck/js/slider.js
+++ webapps/idcheck/js/slider.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 	$('.dropdown').hide();
     	$('div.advanced').click(function () {
-		$('#login').height(400);
+		a = $('.dropdown').height();
+		n =  $('#login').height() + a + 20;
+		$('#login').height(n);
 		$('div.dropdown').slideToggle(400);
 		$('div.advanced').css('visibility','hidden');
     	});
