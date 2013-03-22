87619d0e115556907a0fe3ccc3388cb985029cc4
Calvin J. Sauer <csauer@moore08.cs.purdue.edu>
2013-03-22 18:16:51 -0400

    restfb simple json tests

--- webapps/idcheck/WEB-INF/src/SocialNetworkAcces/FacebookWrapper.java
+++ webapps/idcheck/WEB-INF/src/SocialNetworkAcces/FacebookWrapper.java
@@ -5,19 +5,25 @@ import java.util.ListIterator;
 
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
 import com.restfb.Parameter;
+import com.restfb.json.JsonArray;
+import com.restfb.json.JsonObject;
+import com.restfb.types.Post;
 import com.restfb.types.User;
 
 import ProfileManager.Person;
 import ProfileManager.Profile;
 
 public class FacebookWrapper implements APIWrapper {
+	
+	private static final String FB_ACCESS_TOKEN = "AAACEdEose0cBAGzlhlWxAmAPJwS9mdtIKr8u89hnn5jGGwvRXOyXwkcuyVxyQTYmtZBMgUccnYIBWRK6iagZB5ZBlmyabCtUicfIwXN3wQBZArJp5F9g";
 	//Profile for the person being searched for
 	private DefaultFacebookClient fbClient;
 	private String name;
 	
 	public FacebookWrapper(String name){
-		fbClient = new DefaultFacebookClient("AAACEdEose0cBAGzlhlWxAmAPJwS9mdtIKr8u89hnn5jGGwvRXOyXwkcuyVxyQTYmtZBMgUccnYIBWRK6iagZB5ZBlmyabCtUicfIwXN3wQBZArJp5F9g");
+		fbClient = new DefaultFacebookClient(FB_ACCESS_TOKEN);
 		this.name = name; 
 	}
 	
@@ -40,8 +46,16 @@ public class FacebookWrapper implements APIWrapper {
 	}
 	
 	public static void main(String[] args){
-		FacebookWrapper wrap = new FacebookWrapper("Alex");
-		wrap.findPossibleMatches(null);
+		//FacebookWrapper wrap = new FacebookWrapper("Alex");
+		//wrap.findPossibleMatches(null);
+		FacebookClient client = new DefaultFacebookClient(FacebookWrapper.FB_ACCESS_TOKEN);
+		//Connection<User> userSearch = client.fetchConnection("search", User.class, Parameter.with("q", "Calvin"), Parameter.with("type", "user"));
+		JsonObject usersearch = client.fetchObject("search", JsonObject.class, Parameter.with("q", "Calvin"), Parameter.with("type", "user"));
+		JsonArray userarray = (JsonArray)usersearch.get("data");
+		for(int i = 0; i < userarray.length(); i++){
+			JsonObject user = (JsonObject)userarray.get(i);
+			System.out.println("Found " + user.get("name") + " with id " + user.get("id"));
+		}
 	}
 
 }
